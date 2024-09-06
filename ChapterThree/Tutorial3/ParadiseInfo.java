436436c1216593cbd76037de0d9f9f9c03697be3
NukeTheDev <0727cp@gmail.com>
2024-09-06 03:39:28 +0000

    First four programs for you do it chapter 3

---
+++ ChapterThree/Tutorial3/ParadiseInfo.java
@@ -0,0 +1,14 @@
+package ChapterThree.Tutorial3;
+// Charles "Nuke" Phillips
+// p. 89 Chapter 3 Module 2
+public class ParadiseInfo 
+{
+    public static void main(String[] args) {
+        displayInfo();
+    }
+    public static void displayInfo()
+    {
+        System.out.println("Paradise Day Spa wants to pamper you.");
+        System.out.println("We will make you look good.");
+    }
+}
