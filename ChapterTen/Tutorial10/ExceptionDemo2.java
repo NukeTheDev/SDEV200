ea90826aa0f4bf13687f800e3cad774bcb31be16
NukeTheDev <0727cp@gmail.com>
2024-10-08 04:15:55 +0000

    Ch 10 full add

---
+++ ChapterTen/Tutorial10/ExceptionDemo2.java
@@ -0,0 +1,53 @@
+// Charles "Nuke" Phillips
+// p. 407
+
+package ChapterTen.Tutorial10;
+
+import javax.swing.JOptionPane;
+
+public class ExceptionDemo2
+{
+    public static void main(String[] args) 
+    {
+        int numerator = 0, denominator = 0, result = 0;
+        String inputString;
+        try
+        {
+            inputString = JOptionPane.showInputDialog(null,
+            "Enter a number to be divided");
+            numerator = Integer.parseInt(inputString);
+            inputString = JOptionPane.showInputDialog(null,
+            "Enter a number to divide into the first number");
+            denominator = Integer.parseInt(inputString);
+            
+            // Check for division by zero
+            if (denominator == 0) {
+                // Display warning for division by zero
+                JOptionPane.showMessageDialog(null, 
+                "Division by zero is undefined or results in NaN.");
+            } else {
+                // Perform the division if the denominator is not equal to zero
+                result = numerator / denominator;
+            }
+        }
+        catch(ArithmeticException e)
+        {
+            JOptionPane.showMessageDialog(null, 
+            "Please enter valid integers.");
+        }
+        catch(NumberFormatException exception)
+        {
+            JOptionPane.showMessageDialog(null, 
+            "This application accepts digits only!");
+            numerator = 999;
+            denominator = 999;
+            result = 1;
+        }
+
+        // Display the result if the division was performed
+        if (denominator != 0) {
+            JOptionPane.showMessageDialog(null, numerator + " / " +  
+            denominator + "\nResult is " + result);
+        }
+    }
+}
---
+++ ChapterTen/Tutorial10/PickMenu.java
@@ -0,0 +1,26 @@
+// Charles "Nuke" Phillips
+// p. 424
+package ChapterTen.Tutorial10;
+
+import javax.swing.*;
+
+public class PickMenu 
+{
+    private Menu briefMenu;
+    private String guestChoice = new String();
+    public PickMenu(Menu theMenu)
+    {
+        briefMenu = theMenu;
+        setGuestChoice();
+    }
+    public void setGuestChoice()
+    {
+        JOptionPane.showMessageDialog(null,
+            "Choose from the following menu:");
+        guestChoice = briefMenu.displayMenu();
+    }
+    public String getGuestChoice()
+    {
+        return(guestChoice);
+    }
+}
